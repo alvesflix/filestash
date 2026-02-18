94ccfbaa2cb55c8eb0453561a82a48cb12e9b5ea
Carlos Alves <calves1997@hotmail.com>
2026-02-18 20:18:52 +0000

    fix(plugin): fallback webp thumbnail generation to source image

--- server/plugin/plg_image_c/image_webp.c
+++ server/plugin/plg_image_c/image_webp.c
@@ -5,8 +5,15 @@
 #include "utils.h"
 
 #define WEBP_QUALITY 75
-#define INITIAL_BUFFER_SIZE 1024*64 // 128kB
-#define MAX_BUFFER_SIZE 1024*1024*2 // 2MB
+#define INITIAL_BUFFER_SIZE 1024*64 // 64kB
+#define MAX_BUFFER_SIZE 1024*1024*16 // 16MB
+
+static int passthrough_webp(FILE* output, const uint8_t* data, size_t data_size) {
+  if (data == NULL || data_size == 0) return 1;
+  if (fwrite(data, data_size, 1, output) != 1) return 1;
+  fflush(output);
+  return 0;
+}
 
 int webp_to_webp(int inputDesc, int outputDesc, int targetSize) {
 #ifdef HAS_DEBUG
@@ -57,16 +64,24 @@ int webp_to_webp(int inputDesc, int outputDesc, int targetSize) {
   // STEP2: decode
   int width, height, scale_factor;
   if (!WebPGetInfo(data, data_size, &width, &height)) {
+    int passthrough_status = passthrough_webp(output, data, data_size);
     free(data);
-    ERROR("init");
-    return 1;
+    if (passthrough_status != 0) {
+      ERROR("init");
+      return 1;
+    }
+    return 0;
   }
   DEBUG("init");
   WebPDecoderConfig config;
   if (!WebPInitDecoderConfig(&config)) {
+    int passthrough_status = passthrough_webp(output, data, data_size);
     free(data);
-    ERROR("config");
-    return 1;
+    if (passthrough_status != 0) {
+      ERROR("config");
+      return 1;
+    }
+    return 0;
   }
   scale_factor = (height > targetSize) ? height / targetSize : 1;
   config.options.use_scaling = 1;
@@ -76,11 +91,14 @@ int webp_to_webp(int inputDesc, int outputDesc, int targetSize) {
   DEBUG("config");
   if (WebPDecode(data, data_size, &config) != VP8_STATUS_OK) {
     WebPFreeDecBuffer(&config.output);
+    int passthrough_status = passthrough_webp(output, data, data_size);
     free(data);
-    ERROR("decode");
-    return 1;
+    if (passthrough_status != 0) {
+      ERROR("decode");
+      return 1;
+    }
+    return 0;
   }
-  free(data);
   DEBUG("decode");
 
   // STEP3: encode
@@ -93,14 +111,20 @@ int webp_to_webp(int inputDesc, int outputDesc, int targetSize) {
   );
   if (output_data == NULL) {
     WebPFreeDecBuffer(&config.output);
-    ERROR("encode");
-    return 1;
+    int passthrough_status = passthrough_webp(output, data, data_size);
+    free(data);
+    if (passthrough_status != 0) {
+      ERROR("encode");
+      return 1;
+    }
+    return 0;
   }
   DEBUG("encode");
   fwrite(output_data, output_size, 1, output);
   fflush(output);
   WebPFree(output_data);
   WebPFreeDecBuffer(&config.output);
+  free(data);
   DEBUG("done");
   return status;
 }
